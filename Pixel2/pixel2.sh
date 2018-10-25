e036190d12e1ab7512a99861776f969517514174
Rafael Melo <rsmelo_@hotmail.com>
2018-10-25 14:36:26 -0300

    Scripts to open terminal

---
+++ Pixel2/pixel2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/home/rsmelo/Android/Sdk/emulator/emulator -avd Pixel_2_API_28
+
+exec bash
---
+++ iPhone/iPhone.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/home/rsmelo/Android/Sdk/emulator/emulator -avd iPhone
+
+exec bash
